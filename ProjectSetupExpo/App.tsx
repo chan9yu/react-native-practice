c05ac80e10d1aca0f356c3d27b2ebba1a3a7f61d
chan9yu <dev.cgyeo@gmail.com>
2024-12-10 12:23:14 +0900

    chore: 디렉토리 정리

--- ProjectSetupExpo/App.tsx
+++
@@ -1,5 +0,0 @@
-import WebView from "react-native-webview";
-
-export default function App() {
-  return <WebView source={{ uri: "https://fastcampus.co.kr/" }} />;
-}
--- ProjectSetupReactNativeCli/App.tsx
+++
@@ -1,9 +0,0 @@
-import WebView from 'react-native-webview';
-
-export default function App() {
-  return (
-    <div>
-      <WebView source={{uri: 'https://fastcampus.co.kr/'}} />
-    </div>
-  );
-}
